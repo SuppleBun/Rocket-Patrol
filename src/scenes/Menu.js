d925eb2e3e62a7dc2b08155dd4410835eae1e89e
Joonsu Jun <62852750+SuppleBun@users.noreply.github.com>
2020-04-19 13:24:07 -0700

    reverted code back to original game

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -31,7 +31,7 @@ class Menu extends Phaser.Scene {
         let textSpacer = 64;
 
         // displays high score
-        this.add.text(320, 54, 'HIGH SCORE: '+highScore, menuConfig);
+        this.add.text(269, 54, 'HIGH SCORE: '+highScore, menuConfig);
 
         this.add.text(centerX, centerY - textSpacer, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
         this.add.text(centerX, centerY, 'Use <- -> arrows to move & (F) to Fire', menuConfig).setOrigin(0.5);
@@ -51,7 +51,7 @@ class Menu extends Phaser.Scene {
             // EZ mode
             game.settings = {
                 spaceshipSpeed: 3,
-                gameTimer: 60000
+                gameTimer: 6000
             }
             this.sound.play('sfx_select');
             this.scene.start("playScene");
